1d57ce43c8fd5a161e64d586260c9258a25efd74
abhikhya.ashi@gmail.com <abhikhya.ashi@gmail.com>
2023-10-03 22:54:18 +0530

    Assignment data added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
-
+import WineData from './components/winedata/WineData';
+import WineGammaData from './components/winedata/WineGammaData';
+import { useNavigate } from 'react-router';
+import { Routes, Route } from 'react-router-dom';
 function App() {
+
+  const navigate = useNavigate()
+  const handleFlavonoids = () => {
+    navigate('/wineData')
+  }
+
+  const handleGammaData = () => {
+    navigate('/wineDataByGamma');
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1>Assignment</h1>
+    <button type="button" onClick={handleFlavonoids}>Flavinoids wise Data</button>
+    <button type="button" onClick={handleGammaData}>Gamma wise Data</button>
+        <Routes>
+          <Route exact path="/wineData" Component={WineData}/>
+          <Route exact path="/wineDataByGamma" Component={WineGammaData}/>
+        </Routes>
     </div>
   );
 }
---
+++ src/components/winedata/WineGammaData.js
@@ -0,0 +1,128 @@
+import React, { useEffect, useState } from 'react'
+import wineDetails from './WineData.json';
+
+export default function WineGammaData() {
+    const [classes, setClasses] = useState([]);
+
+    // Function to calculate the mean of an array of numbers
+    function calculateMean(numbers) {
+        if (numbers.length === 0) return 0;
+        const sum = numbers.reduce((acc, val) => acc + val, 0);
+        return sum / numbers.length;
+    }
+
+    // Function to calculate the median of an array of numbers
+    function calculateMedian(numbers) {
+        if (numbers.length === 0) return 0;
+        const sortedNumbers = numbers.slice().sort((a, b) => a - b);
+        const middle = Math.floor(sortedNumbers.length / 2);
+
+        if (sortedNumbers.length % 2 === 0) {
+            const left = sortedNumbers[middle - 1];
+            const right = sortedNumbers[middle];
+            return (left + right) / 2;
+        } else {
+            return sortedNumbers[middle];
+        }
+    }
+
+    // Function to calculate the mode of an array of numbers
+    function calculateMode(numbers) {
+
+        if (numbers.length === 0) return [];
+
+        const frequencyMap = new Map();
+        let maxFrequency = 0;
+
+        for (const number of numbers) {
+            const frequency = (frequencyMap.get(number) || 0) + 1;
+            frequencyMap.set(number, frequency);
+            if (frequency > maxFrequency) {
+                maxFrequency = frequency;
+            }
+        }
+        let mode = null;
+        for (const [number, frequency] of frequencyMap) {
+            if (frequency === maxFrequency) {
+                mode = number;
+            }
+        }
+        return mode;
+    }
+    
+    // Function to calcutate the gamma value of mean median and mode based on class( Alcohal)
+    function calculateClassStatistics(dataset) {
+        const classStatistics = {};
+
+        for (const data of dataset) {
+            const { Alcohol, Ash, Hue, Magnesium } = data;
+            if (!classStatistics[Alcohol]) {
+                classStatistics[Alcohol] = [];
+            }
+            classStatistics[Alcohol].push((Ash * Hue) / Magnesium);
+        }
+        const newData = [];
+
+        for (const className in classStatistics) {
+            const flavanoidsData = classStatistics[className];
+            const mean = calculateMean(flavanoidsData);
+            const roundedMean = mean.toFixed(3);
+
+            const median = calculateMedian(flavanoidsData);
+            const roundedMedian = median.toFixed(3);
+
+            const mode = calculateMode(flavanoidsData);
+            const roundedMode = mode.toFixed(3);
+
+            const data = { class: className, mean: roundedMean, median: roundedMedian, mode: roundedMode };
+            newData.push(data);
+        }
+        setClasses(newData);
+    }
+    useEffect(() => {
+        calculateClassStatistics(wineDetails);
+    }, [])
+
+    return (
+        <div>
+            <h4> Property “Gamma” for each point of the dataset. “Gamma” can be 
+                 calculated as Gamma = (Ash * Hue) / Magnesium.</h4>
+            <table striped bordered hover>
+                <thead>
+                    <tr>
+                        <th>Measure</th>
+                        {classes.map((wine, index) => (
+                            <th>Class {wine.class}</th>
+                        ))}
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Gamma Mean</td>
+                        {classes.map((wine, index) => (
+                            <>
+                                <td>{wine.mean}</td>
+                            </>
+                        ))}
+                    </tr>
+                    <tr>
+                        <td>Gamma Median</td>
+                        {classes.map((wine, index) => (
+                            <>
+                                <td>{wine.median}</td>
+                            </>
+                        ))}
+                    </tr>
+                    <tr>
+                        <td>Gamma Mode</td>
+                        {classes.map((wine, index) => (
+                            <>
+                                <td>{wine.mode}</td>
+                            </>
+                        ))}
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    )
+}
